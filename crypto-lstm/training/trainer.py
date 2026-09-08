bfffd17198802c4d14e93740a9f4d59a8609a68f
ptrbklr <156076085+ptrbklr@users.noreply.github.com>
2026-09-08 16:43:52 +0200

    Revert "let openai fuckup the loader"
    This reverts commit adeaabdf2da88866b918abb7f150d4554a0fe837.

--- crypto-lstm/training/trainer.py
+++ crypto-lstm/training/trainer.py
@@ -80,7 +80,7 @@ class Trainer:
         else:
             df = load_candles(self.cfg.data)
             if self.cfg.data.add_features:
-                df = build_feature_set(df, prefixes=(""), target_prefix="")
+                df = add_technical_features(df)
             target_prefix = None
 
         self.predict_returns = getattr(self.cfg.data, 'predict_returns', False)
